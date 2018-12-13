8d9de16ae320ae1aecb4ed1b7a118c85bc83aa44
altprac <39927035+altprac@users.noreply.github.com>
2018-12-13 14:06:36 +1100

    Update pre.js

--- pre.js
+++ pre.js
@@ -1,4 +1,4 @@
-console.log('env',process.enc.VUE_APP_ENABLE_MAINTENANCE);
+console.log('env',process.env.VUE_APP_ENABLE_MAINTENANCE);
 console.log('prehook', process.env.WEBHOOK_TITLE);
-process.enc.VUE_APP_ENABLE_MAINTENANCE = true
-console.log('env',process.enc.VUE_APP_ENABLE_MAINTENANCE);
+process.env.VUE_APP_ENABLE_MAINTENANCE = true
+console.log('env',process.env.VUE_APP_ENABLE_MAINTENANCE);
